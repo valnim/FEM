13cd49b75dce80364ed4eaacf46752c6a135b8b3
Valentin Nimmervoll <valnim46@gmail.com>
2022-05-12 09:37:17 +0200

    Excercise 2 V1

--- ue/ue3/newton_scalar.py
+++ ue/ue3/newton_scalar.py
@@ -6,8 +6,6 @@
 # #-------------------------------------------------------------------------#
 import numpy as np
 
-# Set the number of equations to solve
-n_equations = 1
 
 # In the scalar case, the function and its derivative can be implemented
 # directly via function handles. For nonlinear equation systems, the
@@ -15,22 +13,64 @@ n_equations = 1
 bsp = 1
 
 if bsp == 0:
+    n_equations = 1
     # x - e^-x
     f = lambda x: x - np.exp(-x)
     df = lambda x: 1 + np.exp(-x)
     x0 = -3
 
 elif bsp == 1:
+    n_equations = 1
     # atan(x)
     f = lambda x: np.arctan(x)
     df = lambda x: 1 / (1 + x**2)
     x0 = np.pi/3  # Good initial guess = pi/3 (converges), bad guess = pi/2 (does not converge, because of instability, overshoots)
 
 elif bsp == 2:
+    n_equations = 1
     # 1/2*x^3-3/2*x^2-1
     f = lambda x: 0.5*x**3-1.5*x**2-1
     df = lambda x: 3/2*x**2-3*x
     x0 = 4  # Good Initial Guess = 4 (converges), Bad Initial Guess = 1 (gets stuck in local maximum)
+
+elif bsp == 3:
+    n_equations = 4
+    # Excercise 2
+
+    # def f(x: np.array([4,1])):
+    #     y = np.zeros([4, 1])
+    #     y[0, 0] = x[0, 0] + x[1, 0] - 2
+    #     y[1, 0] = x[0, 0] * x[2, 0] + x[1, 0] * x[3, 0]
+    #     y[2, 0] = x[0, 0] * x[2, 0]**2 + x[1, 0] * x[3, 0]**2 - 2/3
+    #     y[3, 0] = x[0, 0] * x[2, 0]**3 + x[1, 0] * x[3, 0]**3
+    #     return y
+    def f(x: np.array([4, ])):
+        y = np.zeros([4, ])
+        y[0] = x[0] + x[1] - 2
+        y[1] = x[0] * x[2] + x[1] * x[3]
+        y[2] = x[0] * x[2]**2 + x[1] * x[3]**2 - 2/3
+        y[3] = x[0] * x[2]**3 + x[1] * x[3]**3
+        return y
+
+    def df(x: np.array([4, ])):
+        y = np.zeros([4, 4])
+        y[0, 0] = 1
+        y[0, 1] = 1
+        y[1, 0] = x[2]
+        y[1, 1] = x[3]
+        y[1, 2] = x[0]
+        y[1, 3] = x[1]
+        y[2, 0] = x[2]**2
+        y[2, 1] = x[3]**2
+        y[2, 2] = x[0] * x[2] * 2
+        y[2, 3] = x[1] * x[3] * 2
+        y[3, 0] = x[2]**3
+        y[3, 1] = x[3]**3
+        y[3, 2] = x[0] * x[2]**2 * 3
+        y[3, 3] = x[1] * x[3]**2 * 3
+        return y
+
+    x0 = np.array([1, 3, 4, 5])
 
 
 # Setting the initial guess, the maximum number of iterations and the
@@ -44,26 +84,26 @@ x_arr[:, 0] = x0
 
 # In the following arrays, residuum and increment of each iteration step
 # are stored.
-convergence_residuum = np.ones((1, max_iterations + 1))
-convergence_dx = np.ones((1, max_iterations + 1))
+convergence_residuum = np.ones((n_equations, max_iterations + 1))
+convergence_dx = np.ones((n_equations, max_iterations + 1))
 
 for k in range(max_iterations):
     # Implementation of the Newton Raphson method
     x_k = x_arr[:, k]
-    delta_x = -np.linalg.inv(np.reshape(df(x_k), (n_equations, n_equations))) * np.reshape(f(x_k), (n_equations, n_equations))
-    x_kp1 = x_k + delta_x
+    delta_x = np.matmul(-np.linalg.inv(np.reshape(df(x_k), (n_equations, n_equations))), np.reshape(f(x_k), (n_equations, 1)))
+    x_kp1 = np.reshape(x_k, (n_equations, 1)) + delta_x
     
     # The calculated value is stored
-    x_arr[:, k + 1] = x_kp1
+    x_arr[:, k + 1] = x_kp1[:, 0]
     
     # Residuum and increment are stored for the convergence check. The
     # values are normalized with respect to their initial values.
-    convergence_residuum[0, k + 1] = np.linalg.norm(f(x_kp1))/np.linalg.norm(f(x0))
-    print(convergence_residuum[0, k + 1])
-    convergence_dx[0, k + 1] = np.linalg.norm(delta_x)/np.linalg.norm(x_arr[:, 1]-x0)
+    convergence_residuum[:, k + 1] = np.linalg.norm(f(x_kp1))/np.linalg.norm(f(x0))
+    #print(convergence_residuum[:, k + 1])
+    convergence_dx[:, k + 1] = np.linalg.norm(delta_x)/np.linalg.norm(x_arr[:, 1]-x0)
     
     # Convergence is checked
-    if convergence_residuum[0, k + 1] < convergence_criterion and convergence_dx[0, k + 1] < convergence_criterion:
+    if np.linalg.norm(convergence_residuum[:, k + 1]) < convergence_criterion and np.linalg.norm(convergence_dx[:, k + 1]) < convergence_criterion:
         # If both convergence criteria are fulfilled, we can stop the
         # iteration.
         print('Newton method converged at step ', k)
@@ -76,6 +116,12 @@ for k in range(max_iterations):
         # If there is no convergence after max_iterations iterations, the
         # calculation is aborted and an error is displayed.
         # error('Max Iteration number was reached in Newton-Raphson procedure before convergence was reached')
+        for i in range(k + 1):
+            print('Iteration #', i, ':')
+            print('x =', x_arr[:, i])
+            print('Residuum =', convergence_residuum[0, i])
+            print('Increment =', convergence_dx[0, i])
+            print('')
         raise Exception('Max Iteration number was reached in Newton-Raphson procedure before convergence was reached')
 
 
@@ -87,6 +133,8 @@ for i in range(k + 1):
     print('Increment =', convergence_dx[0, i])
     print('')
 
+
+print('finished')
 
 
 
--- ue/ue3/newton_scalar_with_plot.py
+++ ue/ue3/newton_scalar_with_plot.py
@@ -7,8 +7,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Set the number of equations to solve
-n_equations = 1
 
 # In the scalar case, the function and its derivative can be implemented
 # directly via function handles. For nonlinear equation systems, the
@@ -17,18 +15,21 @@ n_equations = 1
 bsp = 1
 
 if bsp == 0:
+    n_equations = 1
     # x - e^-x
     f = lambda x: x - np.exp(-x)
     df = lambda x: 1 + np.exp(-x)
     x0 = -3
     x = np.linspace(-3.5, 1.5, 1001)
 elif bsp == 1:
+    n_equations = 1
     # atan(x)
     f = lambda x: np.arctan(x)
     df = lambda x: 1 / (1 + x**2)
     x0 = np.pi/3  # Good initial guess = pi/3 (converges), bad guess = pi/2 (does not converge, because of instability, overshoots)
     x = np.linspace(-4, 4, 1001)
 elif bsp == 2:
+    n_equations = 1
     # 1/2*x^3-3/2*x^2-1
     f = lambda x: 0.5*x**3-1.5*x**2-1
     df = lambda x: 3/2*x**2-3*x
@@ -56,11 +57,6 @@ plt.plot([x[0], x[-1]], [0, 0], 'k-')
 plt.plot([x0, x0], [0, f(x0)], 'r')
 plt.legend(['f(x)'])
 
-# In the following arrays, residuum and increment of each iteration step
-# are stored.
-convergence_residuum = np.ones((1, max_iterations + 1))
-convergence_dx = np.ones((1, max_iterations + 1))
-
 for k in range(max_iterations):
     # wait for key press
     #input("Press Enter to continue...")
@@ -78,3 +74,4 @@ for k in range(max_iterations):
     plt.plot(x_kp1[0, 0], f(x_kp1)[0, 0], 'r*')
 
 plt.show()
+print()
